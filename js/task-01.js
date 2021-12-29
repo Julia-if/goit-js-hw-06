8eecab6bb773003ab3d893ffa497882690ea7cdf
Julia-if <juliaif18@gmail.com>
2021-12-29 14:55:48 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesRef = document.querySelector('#categories');
+console.log('Number of categories:', categoriesRef.children.length);
+
+const itemRef = document.querySelector('.item');
+console.log(itemRef);
